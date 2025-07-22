cd5d1d4440ddbadbbc72c4004fe5d452a26bd6f9
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-07-22 22:53:13 +0330

    feat: db write configuration

---
+++ src/Infra/Artix.API.Infra.Sql/Data/Config/Write/CollectionWriteConfiguration.cs
@@ -0,0 +1,46 @@
+﻿namespace Artix.API.Infra.Sql.Data.Config.Write;
+
+using Core.Domain.Entities.Collection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+internal sealed class CollectionWriteConfiguration : BaseEntityConfiguration<Collection>, IEntityTypeConfiguration<Collection>
+{
+    public void Configure(EntityTypeBuilder<Collection> entity)
+    {
+        base.Configure(entity);
+
+        entity.ToTable("Collections");
+
+        entity.Property(e => e.Name)
+            .HasMaxLength(100) 
+            .IsRequired(false);
+
+        entity.Property(e => e.Description)
+            .HasMaxLength(500) 
+            .IsRequired(false);
+
+        entity.Property(e => e.IsPublic)
+            .IsRequired();
+
+        entity.Property(e => e.UserId)
+            .IsRequired();
+
+        entity.HasOne(e => e.User)
+            .WithMany() 
+            .HasForeignKey(e => e.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        entity.HasMany(e => e.Items)
+            .WithOne()
+            .HasForeignKey("CollectionId")
+            .OnDelete(DeleteBehavior.Cascade); 
+
+        entity.HasIndex(e => e.UserId)
+            .HasDatabaseName("IX_Collections_UserId");
+
+        entity.HasIndex(e => e.Name)
+            .HasDatabaseName("IX_Collections_Name")
+            .IsUnique(); 
+    }
+}
