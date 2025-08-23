d688187f262692b7d26776498b53ea31e2b446d6
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-08-23 17:31:53 +0330

    fix: prepare for test development

---
+++ tests/Artix.Test.Functional/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Artix.Test.Functional;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
---
+++ tests/Artix.Test.Security/Test1.cs
@@ -0,0 +1,10 @@
+﻿namespace Artix.Test.Security;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
