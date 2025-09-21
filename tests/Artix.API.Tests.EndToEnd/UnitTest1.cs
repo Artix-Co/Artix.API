6ac1c1a72aaa3d8db16509bcc4f65c07d590c6ea
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-09-21 12:27:11 +0330

    feat: tests folder structures

---
+++ tests/Artix.API.Tests.EndToEnd/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Artix.API.Tests.EndToEnd;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ tests/Artix.API.Tests.Integration/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Artix.API.Tests.Integration;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
