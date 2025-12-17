68c317472664e195c7990d5fcdd3f7128725bb31
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-12-17 14:23:07 +0330

    fix: add migrations

---
+++ src/Infra/Artix.API.Infra.Sql/Migrations/20251217104142_OutboxMessageStatusToEnumType.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Artix.API.Infra.Sql.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutboxMessageStatusToEnumType : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OutboxMessages",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50,
+                oldDefaultValue: "Pending");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "OutboxMessages",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "Pending",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+        }
+    }
+}
