c797effe4de2c67a5b9d685a2853a484c736839b
Mohammad Nazari <m.nazari.dev@icloud.com>
2025-12-30 14:15:45 +0330

    pause remove data

--- src/Presentation/Artix.API.WebService/Program.cs
+++ src/Presentation/Artix.API.WebService/Program.cs
@@ -178,7 +178,7 @@ using (var scope = app.Services.CreateScope())
 
 
     await sqlMigration.MigrateAsync();
-    await sqlDataRemover.Remove();
+    // await sqlDataRemover.Remove();
     await sqlDataSeeder.SeedAsync();
 
 
